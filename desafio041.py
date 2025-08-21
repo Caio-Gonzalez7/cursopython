cf5c6aab63c9a80d13d137a054edc83490d7ed5f
Caio Gonzalez <cnogfogo@gmail.com>
2025-08-21 13:47:27 -0300

    Create desafio041.py

---
+++ desafio041.py
@@ -0,0 +1,23 @@
+from datetime import date
+atual = date.today().year
+
+nasc = int(input('Digite o ano do seu nascimento: '))
+
+idade = atual - nasc
+
+print('O atleta tem {} anos.'.format(idade))
+
+if idade <= 9:
+    print('Classificação: MIRIM')
+
+elif idade <= 14:
+    print('CLassificação: INFANTIL')
+
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+
+elif idade > 25:
+    print('Classificação: MASTER')
