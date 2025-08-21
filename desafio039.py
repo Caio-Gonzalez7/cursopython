ece10339c601e3880ec0f9e6556d4b49767729c2
Caio Gonzalez <cnogfogo@gmail.com>
2025-08-21 10:00:06 -0300

    Update desafio039.py

--- desafio039.py
+++ desafio039.py
@@ -1,11 +1,11 @@
 from datetime import date
-ano = int(input('Digite o ano que você nasceu para saber sobre o seu alistamento! '))
+ano = int(input('Digite o ano que você nasceu: '))
 data = date.today().year
 idade = data - ano
 print('Você tem {} anos'.format(idade))
-if idade <= 17:
-    print('Você ainda não possui a idade minima para se alistar. Ainda faltam {} anos para se alistar'.format(17 - idade))
-elif idade <= 45:
-    print('Você está na idade para se alistar!!')
-elif idade >= 46:
-    print('Você já passou da idade para o alistamento obrigatório. Já se passaram {} anos do prazo'.format(idade - 46))
+if idade < 18:
+    print('Você ainda não possui a idade minima para se alistar. Ainda faltam {} anos para se alistar'.format(18 - idade))
+elif idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade > 18:
+    print('Você deveria ter se alistado hà {} anos.'.format(idade - 18))
