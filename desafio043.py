0f4696467d7b86288de04ad6f715d74303b39a9c
Caio Gonzalez <cnogfogo@gmail.com>
2026-02-23 21:28:52 -0300

    Update desafio043.py

--- desafio043.py
+++ desafio043.py
@@ -3,16 +3,16 @@ altura = float(input('Qual sua altura: '))
 IMC = peso / (altura ** 2)
 
 if IMC <= 18.4:
-    print('Você está abaixo do peso ideal, seu imc é {:.2f}'.format(IMC))
+    print('Você está abaixo do peso ideal, seu IMC é {:.2f}'.format(IMC))
 
 elif IMC <= 24.9:
-    print('Você está no peso ideal, seu imc é {:.2f}'.format(IMC))
+    print('Você está no peso ideal, seu IMC é {:.2f}'.format(IMC))
 
 elif IMC <= 29.9:
-    print('Você esta com sobrepeso, seu imc é {:.2f}'.format(IMC))
+    print('Você esta com sobrepeso, seu IMC é {:.2f}'.format(IMC))
 
 elif IMC <= 39.9:
-    print('Você esta com obesidade, seu imc é {:.2f}'.format(IMC))
+    print('Você esta com obesidade, seu IMC é {:.2f}'.format(IMC))
 
 else:
-    print('Você esta com obesidade mórbida, seu imc é {:.2f}'.format(IMC))
+    print('Você esta com obesidade mórbida, seu IMC é {:.2f}'.format(IMC))aa
