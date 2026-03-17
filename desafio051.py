57e1dc6ff23269b504f4f05de056cc61caacaec0
Caio Gonzalez <cnogfogo@gmail.com>
2026-03-17 19:38:24 -0300

    Create desafio055.py

--- desafio051.py
+++ desafio051.py
@@ -2,4 +2,5 @@ n = int(input('Digite o primeiro termo da PA: '))
 r = int(input('Digite a razão da PA: '))
 for c in range (0, 10):
     PA = n + c * r
-    print(PA)
+    print(PA, end =  ' → ')
+print("ACABOU")
---
+++ desafio055.py
@@ -0,0 +1,2 @@
+for c in range(1, 6):
+    peso = int(input('Digite seu peso: '))
