00ca2c7946168938521c6675bf4015b063251c65
Caio Gonzalez <cnogfogo@gmail.com>
2026-04-15 20:55:12 -0300

    Update desafio052.py

--- desafio052.py
+++ desafio052.py
@@ -1,5 +1,10 @@
 n = int(input('Digite um número: '))
-
-for c in (1, n + 1):
-    n % c == 0
-print(n)
+total = 0
+for c in range(1, n + 1):
+    if n % c == 0:
+        print('\033[34m', end='')
+        total += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end='')
+print(f'\n\033[mO número {n} foi divísivel {total} vezes')
