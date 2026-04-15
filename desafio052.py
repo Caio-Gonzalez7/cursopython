e6fcaee249b9972b425c8ba887f4cbadd0b9af54
Caio Gonzalez <cnogfogo@gmail.com>
2026-04-15 20:37:20 -0300

    Update desafio052.py

--- desafio052.py
+++ desafio052.py
@@ -1,7 +1,5 @@
-n = int(input('Digite um número para saber se é primo: '))
-for c in (2, 23):
-    primo = n % c
-if primo >= 1:
-    print('O numero {} é um numero primo!'.format(n))
-elif primo == 0:
-    print('Não é um numero primo!')
+n = int(input('Digite um número: '))
+
+for c in (1, n + 1):
+    n % c == 0
+print(n)
