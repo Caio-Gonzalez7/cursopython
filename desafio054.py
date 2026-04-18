496bb7a413036b41959cc56fe4bf737f7232fe70
Caio Gonzalez <cnogfogo@gmail.com>
2026-04-18 19:19:39 -0300

    Update desafio054.py

--- desafio054.py
+++ desafio054.py
@@ -1,11 +1,14 @@
-maior = 0
+from datetime import date
+
+atual = date.today().year
 menor = 0
-for c in range(1, 8):
-    ano = int(input('Digite ano do seu nascimento: '))
+maior = 0
+for c in range (1,8):
+    data = int(input(f'Qual o ano a {c}º pessoa nasceu? '))
+    idade = atual - data
     
-    if 2026 - ano >= 18:
-        maior = maior + 1
-    
-    elif 2026 - ano <= 17:
-        menor = menor + 1
-print(f'Das sete pessoas {maior} são maiores de idade e {menor} são menor de idade')
+    if idade >= 21:
+        maior += 1
+    else:
+        menor -= -1
+print(f'Ao todo tivesmo {maior} mariores de idade e {menor} menores de idade! ')
