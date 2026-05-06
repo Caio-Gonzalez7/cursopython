ab091effa866c4379b626ae32b4a0b32762e276a
Caio Gonzalez <cnogfogo@gmail.com>
2026-05-06 15:42:43 -0300

    Update desafio058.py

--- desafio058.py
+++ desafio058.py
@@ -1,9 +1,26 @@
-import random
+from random import randint
 
-lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-escolhido = random.choice(lista)
+computador = randint(0,10)
 
+print('Sou seu computador... Acabei de pensar em um numero de 0 a 10.')
+print('Sera que você consegue adivinhar? ')
 
-while escolhido == n:
-    n = int(input('Digite um numero de 0 a 10 para tentar vencer o computador: '))
-print(escolhido, n )
+acerto = False
+palpites = 0
+
+while not acerto:
+    jogador = int(input('Qual o seu palpite: '))
+    
+    palpites += 1
+    
+    if jogador == computador:
+        acerto = True
+    
+    else:
+        if jogador < computador:
+            print('Mais... Tente novamente: ')
+        
+        if jogador > computador: 
+            print('Menos... Tente novamente: ')
+
+print(f'Você acertou em {palpites} tentativas')
