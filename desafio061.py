9d15059544520eb531fc0bf86ab94de764e63adf
Caio Gonzalez <cnogfogo@gmail.com>
2026-05-11 17:53:55 -0300

    Create desafio061.py

---
+++ desafio061.py
@@ -0,0 +1,10 @@
+p1 = int(input('Primeiro termo: '))
+r = int(input('Razão da PA: '))
+termo = p1
+cont = 1
+
+while cont <= 10:
+    print('{} | '.format(termo), end='')
+    termo += r
+    cont += 1
+print('FIM')
