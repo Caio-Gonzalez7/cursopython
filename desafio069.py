c233ca6472da753b71136096df2463b236fdd1d0
Caio Gonzalez <cnogfogo@gmail.com>
2026-05-26 19:39:06 -0300

    Update desafio069.py

--- desafio069.py
+++ desafio069.py
@@ -1,3 +1,7 @@
+cont_sexo = 0
+cont_idade = 0
+cont_mulheres = 0
+
 while True:
     
     idade = int(input('Idade: '))
@@ -6,7 +10,22 @@ while True:
     while sexo not in 'MF':
         sexo = str(input('Sexo: [M/F] ')).strip().upper()[0] 
     
+    if idade >= 18:
+        cont_idade += 1
+    
+    if sexo == 'M':
+        cont_sexo += 1
+    
+    if sexo == 'F' and idade <= 20:
+        cont_mulheres +=1
+    
     resp = ' '
     while resp not in 'SN':
-        resp = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+        resp = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]    
+    if resp == 'N':
+        break
+
+print(f'Tem {cont_idade} pessoas com mais de 18 anos.')
+print(f'Tem {cont_sexo} homens cadastrados.')
+print(f'Tem {cont_mulheres} mulheres com menos de 20 anos.')
 print('ACABOU!')
