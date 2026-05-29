4ef577441550a9851c67c9bc0aea2aa3f5bdccd9
Caio Gonzalez <cnogfogo@gmail.com>
2026-05-29 16:10:01 -0300

    Create desafio073.py

---
+++ desafio073.py
@@ -0,0 +1,14 @@
+times = ("Palmeiras", "Flamengo", "Fluminense", "Athletico-PR", "Red Bull Bragantino", "Coritiba", "São Paulo", "Bahia", "Cruzeiro", "Botafogo", "Vitória", "Atlético-MG", "Internacional", "Grêmio", "Corinthians", "Vasco", "Santos", "Mirassol", "Remo", "Chapecoense",)
+
+print('\nOs 5 primeiros times são:')
+for time in times [0:6]:
+    print(time)
+
+print('\nOs 4 ultimos times são:')
+for time in times [16:21]:
+    print(time)
+
+print('\nOs times em ordem alfabetica são:')
+print(sorted(times))
+
+print(f'\nO time da Chapecoense está na posição {times.index('Chapecoense')+1}\n')
