5632b612215182420e77c86b0f0eef53ed74d068
Caio Gonzalez <cnogfogo@gmail.com>
2026-06-02 16:49:42 -0300

    Update desafio072.py

--- desafio073.py
+++ desafio073.py
@@ -1,5 +1,8 @@
 times = ("Palmeiras", "Flamengo", "Fluminense", "Athletico-PR", "Red Bull Bragantino", "Coritiba", "São Paulo", "Bahia", "Cruzeiro", "Botafogo", "Vitória", "Atlético-MG", "Internacional", "Grêmio", "Corinthians", "Vasco", "Santos", "Mirassol", "Remo", "Chapecoense",)
 
+print('A tabela atual é:\n')
+print(times)
+
 print('\nOs 5 primeiros times são:')
 for time in times [0:6]:
     print(time)
