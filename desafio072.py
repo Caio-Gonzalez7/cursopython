b552be56a3996e13b1bde42601a6efc1b267ad25
Caio Gonzalez <cnogfogo@gmail.com>
2026-06-02 16:49:42 -0300

    Update desafio072.py

--- desafio072.py
+++ desafio072.py
@@ -1,3 +1,6 @@
 cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 n = int(input('Digite um número entre 0 e 20: '))
+while n < 0 or n > 20:
+    n = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+
 print(f'Você digitou o número {cont[n]}!')
