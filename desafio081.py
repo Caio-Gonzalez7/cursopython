21772425178371dae34adff5cffac54aedd8749d
Caio Gonzalez <cnogfogo@gmail.com>
2026-08-05 22:13:51 -0300

    Update desafio081.py

--- desafio081.py
+++ desafio081.py
@@ -8,8 +8,11 @@ while True:
         break
 
 print(f'Você digitou {len(lista)} elementos!')
+
 lista.sort(reverse=True)
 print(f'Os valores em ordem decrescente {lista}')
+
 if 5 in lista:
     print('O valor 5 faz parte da lista!')
-
+else:
+    print('O valor 5 não foi encontrado na lista!')
