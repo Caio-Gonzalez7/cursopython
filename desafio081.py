b1e16087c869ced2b0c1d48c5ffa455fec41e72e
Caio Gonzalez <cnogfogo@gmail.com>
2026-08-05 22:13:00 -0300

    Create desafio081.py

---
+++ desafio081.py
@@ -0,0 +1,15 @@
+lista = []
+
+while True:
+    lista.append(int(input('Digite um valor: ')))
+
+    resp = str(input('QUER CONTINUAR? [S/N] ')).lower().strip()[0]
+    if resp in 'n':
+        break
+
+print(f'Você digitou {len(lista)} elementos!')
+lista.sort(reverse=True)
+print(f'Os valores em ordem decrescente {lista}')
+if 5 in lista:
+    print('O valor 5 faz parte da lista!')
+
